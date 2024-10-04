a299dc9c2b4cc8cc280da0303df87829d7ec22e3
Artem <gigahod.mm@gmail.com>
2024-10-04 13:29:27 +0500

    ergrg

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,11 +1,9 @@
 "use client"
 
-import Sidebar from "@/components/SideBar";
-
 export default function Dashboard() {
     return (
         <div className="wrapper">
-           <Sidebar/>
+        
         </div>
     );
 }
